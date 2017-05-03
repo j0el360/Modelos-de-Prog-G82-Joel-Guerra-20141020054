dec4fde13def0dfe0131c55ce5ce469294f0ec27
j0el360 <jaencito360@gmail.com>
2017-05-03 13:54:47 -0500

    Add files via upload

---
+++ chain.py
@@ -0,0 +1,42 @@
+
+
+class Manejador:
+    sucesor;
+    def getSucesor:
+        return sucesor;
+    def setSucesor(s):
+        sucesor=s;
+        
+class Manejadorbasico:
+    if(opt==1):
+        print("Liquidacion basica");
+    else:
+
+class ManejadorCatedra:
+    if(opt==2):
+        print("Liquidacion catedra");
+    else:
+
+class Manejadorhc:
+    if(opt==3):
+        print("Liquidacion hc");
+    else:
+
+class Manejadormto:
+    if(opt==4):
+        print("Liquidacion mto");
+    else:
+
+class Manejadortco:
+    if(opt==5):
+        print("Liquidacion tco");
+    else:
+
+class ManejadorHonorarios:
+    if(opt==6):
+        print("Liquidacion honorario");
+    else:
+
+class ManejadorDwefault:
+    print("Opcion no valida");
+    
